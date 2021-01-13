c1b7269ab6f29bccdce8348703774514a9199787
riel20 <66659883+riel20@users.noreply.github.com>
2021-01-13 23:41:58 +0900

    タイトル
    UI表記

---
+++ Apperition.hubs/Assets/scrips/title_light.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class title_light : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Text text;
+    private float time;
+
+    private void Start()
+    {
+        text = this.gameObject.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.color = GetAlphaColor(text.color);
+    }
+
+
+    Color GetAlphaColor(Color color)
+    {
+        time += Time.deltaTime * 5.0f * speed;
+        color.a = Mathf.Sin(time);
+
+        return color;
+    }
+}
